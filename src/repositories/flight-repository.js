ce30e9f04aad1827e078e7bd44a4957d21c0d8ea
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2024-12-16 23:56:48 +0530

    feat : added get all flights method in repository

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,10 +1,19 @@
 const CrudRepository  = require("./crud-repository");
 const {Flights} = require("../models");
+const { where } = require("sequelize");
 
 
 class FligthRepository extends CrudRepository{
        constructor(){
         super(Flights);
+       }
+
+       async getAllFlights(filter , sort){
+        const response = await Flights.findAll({
+              where : filter,
+              order : sort
+        })
+        return response;
        }
 }
 
